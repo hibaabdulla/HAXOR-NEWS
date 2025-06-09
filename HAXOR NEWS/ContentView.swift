d529333810b5c30403ba0e6debeb9ab62d376b51
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-06-08 23:11:18 -0700

    Add List

--- HAXOR NEWS/ContentView.swift
+++ HAXOR NEWS/ContentView.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(posts) { post in Text(post.title)
+            
         }
-        .padding()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [Post(id: "1", title: "Hi"),
+             Post(id: "2", title: "Hlo"),
+             Post(id: "3", title: "Koo")]
 
 #Preview {
     ContentView()
