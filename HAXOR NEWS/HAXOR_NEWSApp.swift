526d8b0e27f04745ab03b005edf06458a39429a1
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-06-08 23:06:14 -0700

    Initial Commit

---
+++ HAXOR NEWS/HAXOR_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAXOR_NEWSApp.swift
+//  HAXOR NEWS
+//
+//  Created by Hiba Abdulla on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HAXOR_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
