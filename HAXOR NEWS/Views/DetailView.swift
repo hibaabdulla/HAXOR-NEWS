852ffd466e3ee4e30d09337a101392c1573b83f9
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-06-09 23:30:16 -0700

    Display WebView Using UIKit Components

---
+++ HAXOR NEWS/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  HAXOR NEWS
+//
+//  Created by Hiba Abdulla on 6/9/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ HAXOR NEWS/Views/WebVIew.swift
@@ -0,0 +1,8 @@
+//
+//  WebVIew.swift
+//  HAXOR NEWS
+//
+//  Created by Hiba Abdulla on 6/9/25.
+//
+
+import Foundation
