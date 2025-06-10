33c2a69708bd1dbda62e2cc39d5108550f7f9fc2
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-06-09 23:30:24 -0700

    Display WebView Using UIKit Components

--- HAXOR NEWS/Views/WebVIew.swift
+++ HAXOR NEWS/Views/WebVIew.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlSrting: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlSrting {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
